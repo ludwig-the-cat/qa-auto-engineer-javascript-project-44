0f9e007e75b55141dfc16b7c0d92444479f083ef
dmitriivqa39 <d.voronets@adguard.com>
2026-02-21 01:16:15 +0500

    step three

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import runGame from '../src/index.js'
+import runGame from '../src/cli.js'
 
 runGame()
--- src/index.js
+++
@@ -1,3 +0,0 @@
-export default function runGame() {
-  console.log('Welcome to the Brain Games!')
-}
